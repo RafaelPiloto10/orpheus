9e1dec17e68726be7aaf85139acd2a5c7ff761aa
Albert Terc <albytterc@protonmail.com>
2023-04-13 12:44:36 -0400

    Gathered genres for all songs in json

--- research/get_genre.py
+++ research/get_genre.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import pandas as pd
+import logging
 from research.spotify_api import get_headers, get_access_token
 
 BASE_URL = "https://api.spotify.com/v1/"
@@ -9,22 +10,22 @@ with open("../dataset/metadata_1681267067.746167.json", "r") as data:
 
 #%%
 auth_token = get_access_token()
+logging.getLogger().setLevel(logging.INFO)
 
+"""
+Must be a list of at most 50 track ids
+"""
 def get_tracks_json(track_ids: list):
     endpoint = "tracks"
     id_string = ",".join(track_ids)
+    logging.info("Getting track jsons")
     resp = requests.get(BASE_URL + endpoint, params={'ids': id_string}, headers=get_headers(auth_token))
     return resp.json()
-
-# def get_tracks_genres(tracks_json: dict):
-#     json_list = tracks_json['tracks']
-#     json_df = pd.DataFrame(json_list)
-#     json_df['genreIncluded'] = json_df.apply(lambda row: contains_genre(row), axis=1)
-#     return json_df
 
 def get_artists_genres(artist_ids: list):
     endpoint = "artists"
     id_string = ",".join(artist_ids)
+    logging.info("Getting artist genres")
     res = requests.get(BASE_URL + endpoint, params={"ids": id_string}, headers=get_headers(auth_token))
     artist_list = res.json()['artists']
     genres = [artist['genres'] for artist in artist_list]
@@ -44,19 +45,23 @@ def _contains_genre(track_json):
 
 
 if __name__ == '__main__':
-    song_ids = []
-    for i in range(50):
-        song_id = songs['results'][i]['id']
-        song_ids.append(song_id)
-    songs_json = get_tracks_json(song_ids)
-    songs_df = pd.json_normalize(songs_json['tracks'])
-    artist_ids = list(get_artist_ids(songs_df['artists']))
-    songs_df['genres'] = get_artists_genres(artist_ids)
-    for i in range(50):
-        songs['results'][i]['genres'] = songs_df['genres'].iloc[i]
+    corpus_df = pd.json_normalize(songs['results'])
+    start = 0
+    end = 50
+    while start < corpus_df.shape[0]:
+        song_ids = list(corpus_df['id'][start:end])
+        songs_json = get_tracks_json(song_ids)
+        songs_df = pd.json_normalize(songs_json['tracks'])
+        artist_ids = list(get_artist_ids(songs_df['artists']))
+        songs_df['genres'] = get_artists_genres(artist_ids)
+
+        for i in range(songs_df.shape[0]):
+            songs['results'][i+start]['genres'] = songs_df['genres'].iloc[i]
+
+        start += 50
+        end += 50
+
 
     with open("with_genres.json", "w+") as fp:
         json.dump(songs, fp)
 
-
-# print(get_track_genre(songs['results'][0]['id']))
