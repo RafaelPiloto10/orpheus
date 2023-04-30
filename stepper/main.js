e478784acd58a1c9c789b63ca1e16a066f2e0722
Rafael Piloto <31188361+RafaelPiloto10@users.noreply.github.com>
2023-04-29 20:58:23 -0400

    data overview

--- stepper/main.js
+++ stepper/main.js
@@ -14,14 +14,37 @@ const back = document.getElementsByClassName("back").item(0);
 const next = document.getElementsByClassName("next").item(0);
 
 const view1 = () => {
+
 	title.textContent = "Overview of Data";
 	subtitle.textContent = "Overview of Data Subtitle";
 
-	content.innerHTML = `
+	disableButtons(true, false); 
+
+	const chart = `
 		<div class="w-full h-full flex justify-center items-center">
-			<p class="text-lg text-white">Overview of Data</p>
+			<img class="w-128 h-128 object-contain" src="assets/views/view1/overview.png" />
 		</div>
 	`;
+
+	content.innerHTML = `
+		<div class="view-1 w-full h-full flex flex-col justify-center items-center" click="">
+			<div class="flex grow items-center justify-centere">
+				<h1 class="text-2xl text-white">
+					In our analysis of gender in song lyrics, we obtained a dataset with 50K songs.
+					We minified our analysis to only observe 6K songs randomly selected from the 50K...
+				</h1>
+			</div>
+
+			<div class="pb-8">
+				<p class="text-white animate-bounce">(click to continue)</p>
+			</div>
+		</div>
+	`;
+
+	content.getElementsByClassName("view-1").item(0).addEventListener("click", () => {
+		enableButtons(true, true);
+		content.innerHTML = chart;
+	});
 }
 
 const introductionView = () => {
@@ -29,8 +52,26 @@ const introductionView = () => {
 	subtitle.textContent = "An analysis of gender on song lyrics";
 
 	content.innerHTML = `
-		<div class="w-full h-full flex justify-center items-center overflow-hidden">
-			<img class="w-64 h-64 object-contain" src="assets/orpheus.png"/>
+		<div class="w-full h-full flex flex-col justify-center items-center overflow-hidden gap-2">
+			<div class="grow flex justify-center items-center">
+				<img class="w-64 h-64 object-contain" src="assets/orpheus.png"/>
+			</div>
+
+			<div class="flex flex-col items-start justify-end gap-2">
+				<div class="flex flex-row items-center justify-center gap-4" >
+					<img class="w-16 h-16 object-cover rounded-full overflow-hidden" src="https://rafaelpiloto10.github.io/assets/img/profile.jpg" />
+					<p class="text-white">
+						Rafael Piloto (<a class="underline text-green-500" href="https://github.com/RafaelPiloto10" target="_blank">@RafaelPiloto10</a>)
+					</p>
+				</div>
+
+				<div class="flex flex-row items-center justify-center gap-4 pb-8" >
+					<img class="w-16 h-16 object-cover rounded-full overflow-hidden" src="https://media.licdn.com/dms/image/C4D03AQF-P2kkatJY9A/profile-displayphoto-shrink_800_800/0/1641077123714?e=1687996800&v=beta&t=Lhbov3rewpqwbV8urwPEdcJKDbXF4lJnxDqlA3LkGcA" />
+					<p class="text-white">
+						Albert Terc (<a class="underline text-green-500" href="https://github.com/albytterc" target="_blank">@albytterc</a>)
+					</p>
+				</div>
+			</div>
 		</div>
 	`;
 }
@@ -53,6 +94,9 @@ const render = () => {
 }
 
 const visitNext = () => {
+	// enables both buttons in case they were disabled on the previous view
+	enableButtons(true, true);
+
 	if (currentView < views.length) {
 		currentView += 1;
 		startTime.textContent = `${currentView}:00`;
@@ -62,11 +106,46 @@ const visitNext = () => {
 }
 
 const visitBack = () => {
+	// enables both buttons in case they were disabled on the previous view
+	enableButtons(true, true);
+
 	if (currentView > 0) {
 		currentView -= 1;
 		startTime.textContent = `${currentView}:00`;
 		progress.style.width = `${(currentView / (views.length - 1)) * 100}%`;
 		render();
+	}
+}
+
+const disableButtons = (f, b) => {
+	if (f) {
+		next.disabled = true;
+		const nextIcon = next.getElementsByClassName("icon").item(0);
+		nextIcon.style.stroke = "gray";
+		nextIcon.classList.remove("hover:fill-white");
+	}
+
+	if (b) {
+		back.disabled = true;
+		const backIcon = back.getElementsByClassName("icon").item(0);
+		backIcon.style.stroke = "gray";
+		backIcon.classList.remove("hover:fill-white");	
+	}
+}
+
+const enableButtons = (f, b) => {
+	if (f) {
+		next.disabled = false;
+		const nextIcon = next.getElementsByClassName("icon").item(0);
+		nextIcon.style.stroke = "white";
+		nextIcon.classList.add("hover:fill-white");
+	}
+
+	if (b) {
+		back.disabled = false;
+		const backIcon = back.getElementsByClassName("icon").item(0);
+		backIcon.style.stroke = "white";
+		backIcon.classList.add("hover:fill-white");
 	}
 }
 
